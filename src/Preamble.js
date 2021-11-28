d164e69d830bc9cd9e10811d9b0909080357e03b
Lily Scott <me@suchipi.com>
2021-11-27 19:37:25 -0700

    Add link to hinzka’s models

--- src/Preamble.js
+++ src/Preamble.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const preamble = `# ARKit Face Blendshapes
+const preamble = `# ARKit Face Blendshapes (Perfect Sync)
 
 This website shows an example of each blendshape that ARKit uses to describe faces. This information can be useful when creating a 3d model you'd like to animate using ARKit; for instance, a model to be used with the "Perfect Sync" feature of [VMagicMirror](https://malaybaku.github.io/VMagicMirror/en/tips/perfect_sync) or [vear](https://apps.apple.com/jp/app/id1490697369).
 
@@ -10,6 +10,8 @@ This website shows an example of each blendshape that ARKit uses to describe fac
 - [ARFaceAnchor.BlendShapeLocation](https://developer.apple.com/documentation/arkit/arfaceanchor/blendshapelocation) documentation
   - This is where I got the images from.
 - [iPhone トラッキング向け BlendShape リスト](https://hinzka.hatenablog.com/entry/2020/06/15/072929)
+- [@hinzka/52blendshapes-for-VRoid-face](https://github.com/hinzka/52blendshapes-for-VRoid-face)
+  - This repository contains modified versions of [VRoid Studio](https://vroid.com/en/studio) models with these blend shapes added, distributed in [VRM format](https://vrm.dev/en/). These models can be used as bases for your own VRoid Studio avatars, in order to enable Perfect Sync.
 - [VMagicMirror Perfect Sync Tips](https://malaybaku.github.io/VMagicMirror/en/tips/perfect_sync)
 - [iPhoneMoCapiOS](https://github.com/johnjcsmith/iPhoneMoCapiOS)
 - [How to create Apple’s Animoji using ARKit?](https://medium.com/@ashutoshdingankar/how-to-create-apples-animoji-using-arkit-4691e04bfc42)
